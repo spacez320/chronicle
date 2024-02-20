ed98b723e20984bfa7fe6e61c3955f4eb1e4f215
Matthew Coleman <matthew@eudaimonia.io>
2024-02-20 10:18:10 -0500

    Make history display correctly (#2)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import aiobungie
 import asyncio
 import click
+import datetime
 import logging
 import os
 from rich.console import Console
 from rich.logging import RichHandler
 from rich.table import Table
+from rich.pretty import pprint
 
 
 # Global variables. 😬
@@ -13,6 +15,39 @@ BUNGIE_API_KEY = os.getenv("BUNGIE_API_KEY")
 PLAYER_CODE = os.getenv("PLAYER_CODE")
 PLAYER_MEMBERSHIP_TYPE = aiobungie.MembershipType.PSN
 PLAYER_NAME = os.getenv("PLAYER_NAME")
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option(
+    "--guardian-class",
+    default="warlock",
+    prompt="Guardian class",
+    help="Guardian class to inspect.",
+)
+def history(guardian_class):
+    """Gets various historical stats."""
+
+    def _map_history(i):
+        """Munge history output into something displayable."""
+        return {
+            "basic": i["basic"]["displayValue"],
+            "pga": i["pga"]["displayValue"] if "pga" in i else "n/a",
+            "statId": i["statId"],
+        }
+
+    chronicle = Chronicle(PLAYER_NAME, PLAYER_CODE, PLAYER_MEMBERSHIP_TYPE)
+    history = asyncio.run(chronicle.get_player_history(guardian_class))
+
+    for activity_mode in history:
+        create_table(
+            list(map(_map_history, list(history[activity_mode]["allTime"].values()))),
+            activity_mode.title(),
+        )
 
 
 @click.command()
@@ -36,6 +71,7 @@ class Chronicle:
         self.name = name
 
         self.client = aiobungie.Client(BUNGIE_API_KEY)
+        self.rest_client = aiobungie.RESTClient(BUNGIE_API_KEY)
         self.player = asyncio.run(self._get_player())
         self.player_profile = asyncio.run(self._get_player_profile())
 
@@ -72,6 +108,29 @@ class Chronicle:
 
         return player_profile
 
+    async def get_player_history(self, guardian_class):
+        """Fetch historical data for a player."""
+        character = next(
+            filter(
+                lambda character: character.class_type
+                == Chronicle.get_class_from_str(guardian_class),
+                self.player_profile.characters.values(),
+            )
+        )
+
+        async with self.rest_client:
+            history = await self.rest_client.fetch_historical_stats(
+                character.id,
+                self.player.id,
+                self.membership_type,
+                day_start=datetime.datetime.min,
+                day_end=datetime.datetime.max,
+                groups=[aiobungie.internal.enums.GroupType.GENERAL],
+                modes=[aiobungie.internal.enums.GameMode.ALLPVE],
+            )
+
+        return history
+
     async def get_weapons_by_class(self, guardian_class):
         """Fetch weapon data for a specific player class."""
         character = next(
@@ -90,17 +149,32 @@ class Chronicle:
         return weapons
 
 
-def create_table(data, title):
-    """Prints a rich table from an arbitrary object."""
+def create_table(data, title, columns=None):
+    """Prints a rich table from an arbitrary object or dictionary."""
     console = Console()
     table = Table(title=title)
 
-    columns = [k for k in dir(data[0]) if not k.startswith("_")]
+    columns = (
+        (
+            data[0].keys()
+            if type(data[0]) is dict
+            else [k for k in dir(data[0]) if not k.startswith("_")]
+        )
+        if columns is None
+        else columns
+    )
+
     for column in columns:
         table.add_column(column)
 
-    for d in data:
-        table.add_row(*[str(getattr(d, column)) for column in columns])
+    for dat in data:
+        row = (
+            list(dat.values())
+            if type(dat) is dict
+            else [str(getattr(dat, column)) for column in columns]
+        )
+
+        table.add_row(*[str(v) for v in row])
 
     console.print(table)
 
@@ -114,4 +188,6 @@ if __name__ == "__main__":
         handlers=[RichHandler(markup=True)],
     )
 
-    weapons()
+    cli.add_command(weapons)
+    cli.add_command(history)
+    cli()
