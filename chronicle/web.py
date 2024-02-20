5ebbcc9a207cbf1a7cc7035b32b6f16da229ebdb
Matthew Coleman <matthew@eudaimonia.io>
2024-02-20 16:31:44 -0500

    POC htmx

--- chronicle/web.py
+++ chronicle/web.py
@@ -9,3 +9,11 @@ def init_web(app, chronicle):
     @app.route("/")
     def hello():
         return render_template("index.html")
+
+    @app.route("/history")
+    def history():
+        return "<div>History called!</div>"
+
+    @app.route("/weapons")
+    def weapons():
+        return "<div>Weapons called!</div>"
