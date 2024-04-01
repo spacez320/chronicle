80830fd991707cddf20190f64d6d0d3ad5a652f5
Matthew Coleman <matthew@eudaimonia.io>
2024-04-01 15:29:01 -0400

    Better loading, error management for /weapons

--- chronicle/web.py
+++ chronicle/web.py
@@ -42,12 +42,15 @@ def init_web(app, chronicle):
     @app.route("/weapons", methods=["POST"])
     def weapons():
         """Gets weapon usage."""
-        player_name, player_code = request.form["id"].split("#")
-        player_class = request.form["class"]
-        player_membership_type = request.form["membership"]
+        try:
+            player_name, player_code = request.form["id"].split("#")
+            player_class = request.form["class"]
+            player_membership_type = request.form["membership"]
 
-        chronicle.init_player(player_name, player_code, player_membership_type)
+            chronicle.init_player(player_name, player_code, player_membership_type)
 
-        return create_html_table(
-            asyncio.run(chronicle.get_weapons_by_class(player_class)), "Weapons"
-        )
+            return create_html_table(
+                asyncio.run(chronicle.get_weapons_by_class(player_class)), "Weapons"
+            )
+        except Exception as e:
+            return create_html_error(e)
